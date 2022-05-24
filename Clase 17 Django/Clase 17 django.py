079e683860e160aa1682e5db773cb7458e7a8075
julian1306 <julian_1306@hotmail.com>
2022-05-24 12:12:08 -0300

    Clase 17

---
+++ Clase 17 Django/Clase 17 django.py
@@ -0,0 +1,14 @@
+#instalacion Django .
+
+# Correr el terminal desde la carpeta don de estemos marados en este c aso la de Clase 17 Django
+#- PS G:\Mi unidad\Curso Python Files\Clase 17 Django> pip install django
+#-PS G:\Mi unidad\Curso Python Files\Clase 17 Django> django-admin startproject proyecto_x
+#-PS G:\Mi unidad\Curso Python Files\Clase 17 Django> cd .\proyecto_x\
+#-PS G:\Mi unidad\Curso Python Files\Clase 17 Django\proyecto_x> python manage.py migrate
+
+#con este crea el server local en http://127.0.0.1:8000/ o local host
+
+# - PS G:\Mi unidad\Curso Python Files\Clase 17 Django\proyecto_xx> python manage.py runserver
+
+#admin por default
+#-http://127.0.0.1:8000/admin/login/?next=/admin/
