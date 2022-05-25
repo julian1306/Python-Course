853f50d9aed5f076b337247d151a2426c2b8c862
julian1306 <julian_1306@hotmail.com>
2022-05-25 12:57:44 -0300

    fecha nacimiento

--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/models.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/models.py
@@ -10,5 +10,6 @@ class Familiares(models.Model):
     apellido = models.CharField(max_length=100)
     edad = models.FloatField(max_length=200, blank=True, null=True)
     dni = models.FloatField(max_length=8)
+    fecha_nacimiento = models.CharField(max_length=100,blank=True, null=True)
     
 
--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
@@ -2,7 +2,6 @@ from multiprocessing import context
 from unicodedata import name
 from django.shortcuts import render
 from datetime import datetime
-from models import Familiares
 from familiares.models import Familiares
 
 
@@ -14,21 +13,21 @@ def familiares(request):
         apellido = "Garcia",
         edad = 60,
         dni = 17852974,
-        fecha_nacimiento= "07/06/1962"
+        fecha_nacimiento = "07/06/1962",
     )
     familiar_nuevo2 = Familiares.objects.create(
         nombre = "Veronica",
         apellido = "Salmoiraghi",
         edad = 57,
         dni = 119587634,
-        fecha_nacimiento= "30/10/1971"
+        fecha_nacimiento = "30/10/1971",
     )
     familiar_nuevo3 = Familiares.objects.create(
         nombre = "Nicolas",
         apellido = "Vivas",
         edad = 15,
         dni = 52635898,
-        fecha_nacimiento= "15/08/2007"
+        fecha_nacimiento = "15/08/2007",
     )
 
     context = { "familiar_nuevo":familiar_nuevo, "familiar_nuevo2":familiar_nuevo2, "familiar_nuevo3":familiar_nuevo3}
