e168c49045456a9beb26e8ae920e3a5f340f5129
julian1306 <julian_1306@hotmail.com>
2022-05-25 09:16:50 -0300

    fecha nac

--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
@@ -1,7 +1,9 @@
 from multiprocessing import context
+from unicodedata import name
 from django.shortcuts import render
-from familiares.models import Familiares
 from datetime import datetime
+from models import Familiares
+from familiares.models import Familiares
 
 
 # Create your views here.
@@ -12,22 +14,25 @@ def familiares(request):
         apellido = "Garcia",
         edad = 60,
         dni = 17852974,
+        fecha_nacimiento= "07/06/1962"
     )
     familiar_nuevo2 = Familiares.objects.create(
         nombre = "Veronica",
         apellido = "Salmoiraghi",
         edad = 57,
         dni = 119587634,
+        fecha_nacimiento= "30/10/1971"
     )
     familiar_nuevo3 = Familiares.objects.create(
         nombre = "Nicolas",
         apellido = "Vivas",
         edad = 15,
         dni = 52635898,
+        fecha_nacimiento= "15/08/2007"
     )
 
     context = { "familiar_nuevo":familiar_nuevo, "familiar_nuevo2":familiar_nuevo2, "familiar_nuevo3":familiar_nuevo3}
     return render(request, "familiares.html", context=context)
 
 
-   
+    
