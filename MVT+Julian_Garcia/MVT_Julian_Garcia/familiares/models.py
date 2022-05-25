8513ec908ac16b1062c414347f7bad2caf77bb41
julian1306 <julian_1306@hotmail.com>
2022-05-25 17:41:02 -0300

    Arreglo en models integerfield

--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/models.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/models.py
@@ -8,8 +8,8 @@ from django.db import models
 class Familiares(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    edad = models.FloatField(max_length=200, blank=True, null=True)
-    dni = models.FloatField(max_length=8)
-    fecha_nacimiento = models.CharField(max_length=100,blank=True, null=True)
+    edad = models.IntegerField()
+    dni = models.IntegerField()
+    fecha_nacimiento = models.CharField(max_length=100)
     
 
