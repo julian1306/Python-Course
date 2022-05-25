3c6466a23ab73c4a062050d9318ba498a704065e
julian1306 <julian_1306@hotmail.com>
2022-05-25 18:01:54 -0300

    borrar en view general al pedo

--- MVT+Julian_Garcia/MVT_Julian_Garcia/MVT_Julian_Garcia/views.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/MVT_Julian_Garcia/views.py
@@ -4,21 +4,3 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
-
-def saludo(request):
-    return
-
-
-
-
-
-
-
-def return_template(request):
-    context = {
-
-    }
-
-
-    return render(request, 'template_1.html', context = context)
