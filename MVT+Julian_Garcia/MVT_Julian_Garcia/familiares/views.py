6483ff43fb9e768d66a72fe97a5551d1878bae13
julian1306 <julian_1306@hotmail.com>
2022-05-28 22:36:17 -0300

    Revert "db"
    This reverts commit b9929bca5df09187c1df4de797fb00b43868542d.

--- MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
+++ MVT+Julian_Garcia/MVT_Julian_Garcia/familiares/views.py
@@ -7,6 +7,27 @@ from familiares.models import Familiares
 # Create your views here.
 
 def familiares(request):
+    familiar_nuevo = Familiares.objects.create(
+        nombre = "Gabriel",
+        apellido = "Garcia",
+        edad = 60,
+        dni = 17852974,
+        fecha_nacimiento = "07/06/1962",
+    )
+    familiar_nuevo2 = Familiares.objects.create(
+        nombre = "Veronica",
+        apellido = "Salmoiraghi",
+        edad = 57,
+        dni = 119587634,
+        fecha_nacimiento = "30/10/1971",
+    )
+    familiar_nuevo3 = Familiares.objects.create(
+        nombre = "Nicolas",
+        apellido = "Vivas",
+        edad = 15,
+        dni = 52635898,
+        fecha_nacimiento = "15/08/2007",
+    )
     familiares_all = Familiares.objects.all()
 
 
