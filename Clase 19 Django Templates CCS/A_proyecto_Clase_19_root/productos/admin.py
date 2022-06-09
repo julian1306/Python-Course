bdc2942d7805127615d9b229f21bf7c374aa5dd6
julian1306 <julian_1306@hotmail.com>
2022-06-09 10:41:20 -0300

    details

--- Clase 19 Django Templates CCS/A_proyecto_Clase_19_root/productos/admin.py
+++ Clase 19 Django Templates CCS/A_proyecto_Clase_19_root/productos/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from productos.models import Productos # Aca le hago import de la clase/ modelo Productos
+
+
+
 # Register your models here.
 
-admin.site.register(Productos)   # Aca registro los productos para laburarlos del portal de admin
+#admin.site.register(Productos)   # Aca registro los productos para laburarlos del portal de admin
+
+@admin.register(Productos)   # Nueva forma de registrarlo 
+class ProductosAdmin(admin.ModelAdmin):  # forma para que te muestre lo que quieras en el admin site 
+    list_display = ["name", "price", "SKU", "available"] 
 
 
--- Clase 19 Django Templates CCS/A_proyecto_Clase_19_root/productos/urls.py
+++ Clase 19 Django Templates CCS/A_proyecto_Clase_19_root/productos/urls.py
@@ -2,7 +2,7 @@
 #URLS dentro de http://127.0.0.1:8000/productos/
 
 from django.urls import path, include
-from productos.views import productos_all,create_product, search_product    # importo las def de views para laburar las URL 
+from productos.views import productos_all,create_product, search_product, detail_product    # importo las def de views para laburar las URL 
 
 
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path("listar_productos/", productos_all , name = "listar_productos"),
     path("crear_productos/", create_product, name="crear_productos"),  # Aca creo las urls de products y llamo a las views de product 
     path("buscar_productos/", search_product, name = "buscar_productos"),
+    path("product_detail/<int:pk>/", detail_product, name = "product_detail"), # aca con el <int:id> aclaramos que le vamos a pasar un entero llamado pk 
     
 ]
 
