c3f88f446923b00ebaabadf3a413864f17c0a4f5
julian1306 <julian_1306@hotmail.com>
2022-07-05 15:26:47 -0300

    level deafult 0 , lo  saque del register

--- Clase 23- 24 Django avanzado users/Entrega1+JulianGarcia/Entrega1_MVT/Entrega1_MVT/forms.py
+++ Clase 23- 24 Django avanzado users/Entrega1+JulianGarcia/Entrega1_MVT/Entrega1_MVT/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import fields
+from email.policy import default
 from msilib.schema import Class
 from tkinter import Widget
 from django import forms 
@@ -12,11 +13,10 @@ class User_registration_form(UserCreationForm):
     email = forms.EmailField()
     password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Repita su Contraseña", widget=forms.PasswordInput)
-    level = forms.IntegerField()
 
     class Meta:
         model = User
-        fields = ["username","email" , "password1", "password2","level"]
+        fields = ["username","email" , "password1", "password2"]
         help_texts = {k: "" for k in fields}                   # for re loco para que no muestre los help text 
 
 
